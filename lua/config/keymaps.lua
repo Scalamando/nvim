78d69690d9d670f35ca2e38d9624caf448d42e69
Raimund Canzler <git@rai-canzler.de>
2023-12-19 00:18:57 +0100

    edit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- remove lazyterm bindings
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
+vim.keymap.del("n", "<c-/>")
+
+-- toggleterm (bottom)
+vim.keymap.set("n", "<A-n>", function()
+  vim.cmd.ToggleTerm('direction="horizontal"')
+end, { desc = "Toogle Terminal" })
+vim.keymap.set("t", "<A-n>", function()
+  vim.cmd.ToggleTerm('direction="horizontal"')
+end, { desc = "Toogle Terminal" })
+
+-- toggleterm (floating)
+vim.keymap.set("n", "<A-i>", function()
+  vim.cmd.ToggleTerm('direction="float"')
+end, { desc = "Toogle Terminal (floating)" })
+vim.keymap.set("t", "<A-i>", function()
+  vim.cmd.ToggleTerm('direction="float"')
+end, { desc = "Toogle Terminal (floating)" })
