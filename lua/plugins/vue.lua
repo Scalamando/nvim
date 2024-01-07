e21e4aef9287aec21672bade7b0144d18d68db0c
Raimund Canzler <git@rai-canzler.de>
2024-01-07 01:50:52 +0100

    feat: enable volar conditionally with neoconf

--- lua/plugins/vue.lua
+++ lua/plugins/vue.lua
@@ -1,8 +1,22 @@
+local neoconf_defaults = {
+	volar = {
+		enable = false,
+	}
+}
+
+local function is_volar_enabled()
+  return require("neoconf").get("volar", neoconf_defaults).enable
+end
+
 return {
   -- add vue to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
+      if not is_volar_enabled() then
+        return
+      end
+
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "vue", "typescript", "javascript", "css" })
       end
@@ -19,6 +33,18 @@ return {
         volar = {
           filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
         },
+      },
+      setup = {
+        volar = function()
+          if not is_volar_enabled() then
+            return true
+          end
+        end,
+        tsserver = function()
+          if is_volar_enabled() then
+            return true
+          end
+        end,
       },
     },
   },
