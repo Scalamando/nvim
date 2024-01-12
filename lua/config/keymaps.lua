51a1ef84ae4d21ab2727d7c29569298df2c423fb
Raimund Canzler <git@rai-canzler.de>
2024-01-12 12:31:13 +0100

    feat: move toggleterm key mappings into config for numbered terminals support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,23 +7,6 @@ vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
 vim.keymap.del("n", "<c-/>")
 
--- toggleterm (bottom)
-vim.keymap.set("n", "<A-n>", function()
-  vim.cmd.ToggleTerm('direction="horizontal"')
-end, { desc = "Toogle Terminal" })
-vim.keymap.set("t", "<A-n>", function()
-  vim.cmd.ToggleTerm('direction="horizontal"')
-end, { desc = "Toogle Terminal" })
-
--- toggleterm (floating)
-vim.keymap.set("n", "<A-i>", function()
-  vim.cmd.ToggleTerm('direction="float"')
-end, { desc = "Toogle Terminal (floating)" })
-vim.keymap.set("t", "<A-i>", function()
-  vim.cmd.ToggleTerm('direction="float"')
-end, { desc = "Toogle Terminal (floating)" })
-
-
 -- center view after jumps
 vim.keymap.set("n", "<C-d>", "<C-d>", { desc = "Jump half page down" })
 vim.keymap.set("n", "<C-u>", "<C-u>", { desc = "Jump half page up" })
