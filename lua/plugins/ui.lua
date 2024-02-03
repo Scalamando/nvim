663affff180bbeb0197c95344ca84b7ef7b56a7b
Raimund Canzler <git@rai-canzler.de>
2024-02-03 12:47:40 +0100

    refactor: restructure plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,4 +20,28 @@ return {
       },
     },
   },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<C-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+        shell = "zsh --login",
+      })
+    end,
+    keys = {
+      { [[<C-\>]] },
+      {
+        "<A-n>",
+        function()
+          local count = vim.v.count1
+          require("toggleterm").toggle(count, 10, vim.loop.cwd(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal)",
+      },
+    },
+  },
 }
