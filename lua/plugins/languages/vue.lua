a941e646725f3779db7709d476b095590bf118bb
Raimund Canzler <git@rai-canzler.de>
2024-03-15 11:03:41 +0100

    feat: use new volar 2.0 typescript plugin

--- lua/plugins/languages/vue.lua
+++ lua/plugins/languages/vue.lua
@@ -1,22 +1,8 @@
-local neoconf_defaults = {
-	volar = {
-		enable = false,
-	}
-}
-
-local function is_volar_enabled()
-  return require("neoconf").get("volar", neoconf_defaults).enable
-end
-
 return {
   -- add vue to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      if not is_volar_enabled() then
-        return
-      end
-
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "vue", "typescript", "javascript", "css" })
       end
@@ -26,26 +12,34 @@ return {
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        ---@type lspconfig.options.volar
-        volar = {
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    opts = function(_, opts)
+      local mason_registry = require("mason-registry")
+      local has_volar, volar = pcall(mason_registry.get_package, "vue-language-server")
+      local vue_ts_plugin_path = has_volar
+        and volar:get_install_path() .. "/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
+      -- after volar 2.0.7
+      -- local vue_ts_plugin_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/typescript-plugin'
+
+      ---@type lspconfig.options.volar
+      opts.servers.volar = {} -- make sure mason installs the server
+
+      ---@type lspconfig.options.tsserver
+      opts.servers.tsserver = {
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_ts_plugin_path, 
+              languages = { "vue" },
+            },
+          },
         },
-      },
-      setup = {
-        volar = function()
-          if not is_volar_enabled() then
-            return true
-          end
-        end,
-        tsserver = function()
-          if is_volar_enabled() then
-            return true
-          end
-        end,
-      },
-    },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      }
+    end,
   },
 }
