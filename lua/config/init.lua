98cce4f7e26571f7d833917c63cd0b4b5ef784ba
Raimund Canzler <git@rai-canzler.de>
2024-12-26 22:35:57 +0100

    format

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 -- Add support for the LazyFile event
 local Event = require 'lazy.core.handler.event'
 
-Event.mappings.LazyFile = { id = 'LazyFile', event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
+Event.mappings.LazyFile = { id = 'LazyFile', event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' } }
 Event.mappings['User LazyFile'] = Event.mappings.LazyFile
 
 require 'config.options'
