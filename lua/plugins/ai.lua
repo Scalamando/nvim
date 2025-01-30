c6a98aca920bd93e403de02258cce80d1516ad18
Raimund Canzler <git@rai-canzler.de>
2025-01-31 00:07:48 +0100

    add codecompanion.nvim keymaps

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,6 +6,26 @@ return {
       'nvim-treesitter/nvim-treesitter',
     },
     lazy = false,
+    keys = {
+      {
+        '<leader>A',
+        '<cmd>CodeCompanionActions<cr>',
+        mode = { 'n', 'v' },
+        { noremap = true, silent = true },
+      },
+      {
+        '<leader>a',
+        '<cmd>CodeCompanionChat Toggle<cr>',
+        mode = { 'n', 'v' },
+        { noremap = true, silent = true },
+      },
+      {
+        'ga',
+        '<cmd>CodeCompanionChat Add<cr>',
+        mode = 'v',
+        { noremap = true, silent = true },
+      },
+    },
     opts = {
       strategies = {
         chat = { adapter = 'ollama' },
