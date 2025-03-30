879ead304b8ae33d2b5b931ba0c7266ffb760568
Raimund Canzler <git@rai-canzler.de>
2025-03-30 16:13:32 +0200

    fix: remove nodejs from runtime tools
    This fixes local nodejs versions being shadowed by the nvim version.

--- flake.nix
+++ flake.nix
@@ -56,7 +56,6 @@
           gotools
           impl
           just
-          nodejs_20
           nodePackages.prettier
           vscode-langservers-extracted
           php84Packages.php-codesniffer
