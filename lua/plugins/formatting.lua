f7bc30e6bbb8e7bb480f530b8dadec46339617e5
Raimund Canzler <git@rai-canzler.de>
2025-07-04 12:00:59 +0200

    feat: cleanup formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -6,19 +6,24 @@ return {
       {
         '<leader>cf',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format { async = true }
         end,
-        mode = '',
-        desc = 'Format buffer',
+        mode = { 'n', 'v' },
+        desc = 'Format Code',
+      },
+      {
+        '<leader>cF',
+        function()
+          require('conform').format { formatters = { 'injected' }, timeout_ms = 3000 }
+        end,
+        mode = { 'n', 'v' },
+        desc = 'Format Injected Langs',
       },
     },
     opts = {
-      notify_on_error = false,
       default_format_opts = {
+        lsp_format = 'fallback',
         timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-        lsp_format = 'fallback', -- not recommended to change
       },
       formatters_by_ft = {
         json = { 'prettier' },
