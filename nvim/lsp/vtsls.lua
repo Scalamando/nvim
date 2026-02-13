e398dbe55104f11315c24c84a894ce5a9e6f6dd2
Raimund Canzler <git@rai-canzler.de>
2026-02-13 11:56:46 +0100

    feat: use snacks keymap for vtsls keymaps

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -54,26 +54,27 @@ return {
     end
 
     local lsp = require 'config.lsp'
-    vim.keymap.set('n', 'gd', function()
-      local params = vim.lsp.util.make_position_params()
+    Snacks.keymap.set('n', 'gD', function()
+      local win = vim.api.nvim_get_current_win()
+      local params = vim.lsp.util.make_position_params(win, client.offset_encoding or 'utf-8')
       lsp.execute {
-        command = 'typescript.gotosourcedefinition',
+        command = 'typescript.goToSourceDefinition',
         arguments = { params.textDocument.uri, params.position },
         open = true,
       }
     end, { desc = 'goto source definition', buffer = true })
-    vim.keymap.set('n', 'gr', function()
+    Snacks.keymap.set('n', 'gR', function()
       lsp.execute {
-        command = 'typescript.findallfilereferences',
+        command = 'typescript.findAllFileReferences',
         arguments = { vim.uri_from_bufnr(0) },
         open = true,
       }
     end, { desc = 'file references', buffer = true })
-    vim.keymap.set('n', '<leader>co', lsp.action['source.organizeimports'], { desc = 'organize imports', buffer = true })
-    vim.keymap.set('n', '<leader>cm', lsp.action['source.addmissingimports.ts'], { desc = 'add missing imports', buffer = true })
-    vim.keymap.set('n', '<leader>cu', lsp.action['source.removeunused.ts'], { desc = 'remove unused imports', buffer = true })
-    vim.keymap.set('n', '<leader>ce', lsp.action['source.fixall.ts'], { desc = 'fix all diagnostics', buffer = true })
-    vim.keymap.set('n', '<leader>cV', function()
+    Snacks.keymap.set('n', '<leader>co', lsp.action['source.organizeImports'], { desc = 'organize imports', buffer = true })
+    Snacks.keymap.set('n', '<leader>cm', lsp.action['source.addMissingImports.ts'], { desc = 'add missing imports', buffer = true })
+    Snacks.keymap.set('n', '<leader>cu', lsp.action['source.removeUnused.ts'], { desc = 'remove unused imports', buffer = true })
+    Snacks.keymap.set('n', '<leader>ce', lsp.action['source.fixAll.ts'], { desc = 'fix all diagnostics', buffer = true })
+    Snacks.keymap.set('n', '<leader>cV', function()
       lsp.execute { command = 'typescript.selectTypeScriptVersion' }
     end, { desc = 'Select TS workspace version', buffer = true })
   end,
